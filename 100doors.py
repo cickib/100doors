7e4a8bf7b8c3b9ec41c58be14b0cfb5cd0843803
cickib <bellakocsis06@gmail.com>
2016-05-17 11:35:06 +0200

    100doors.py

--- 100doors.py
+++ 100doors.py
@@ -1,4 +1,17 @@
-print("These doors are open after the 100th cycle:")
-for n in range(1, 11):
-    square = n*n
-    print(square)
+print("The # of the doors open after the 100th cycle:")
+
+doors = []
+for i in range(1, 101):
+    doors.append(0)
+
+for y in range(1, 101):
+    for x in range(0,100):
+        if (x+1) % y == 0:
+            if doors[x] == 0:
+                doors[x] = 1
+            else:
+                doors[x] = 0
+
+for x in range(0,100):
+    if doors[x] == 1:
+        print(x+1)
---
+++ 100doors_math.py
@@ -0,0 +1,4 @@
+print("These doors are open after the 100th cycle:")
+for n in range(1, 11):
+    square = n*n
+    print(square)
