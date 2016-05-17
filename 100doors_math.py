c6016af9362b4f9268923fde7e6a75fd33638419
Bella Kocsis <bellakocsis06@gmail.com>
2016-05-17 11:49:09 +0200

    Update 100doors_math.py

--- 100doors_math.py
+++ 100doors_math.py
@@ -1,4 +1,5 @@
-print("These doors are open after the 100th cycle:")
+print("These doors are open after the 100th cycle:", end="")
 for n in range(1, 11):
     square = n*n
-    print(square)
+    print(" ", square, end="")
+print("")
