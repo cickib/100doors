5dea778b79bdb65d40ddd537c8cd7e656c5cbd52
Bella Kocsis <bellakocsis06@gmail.com>
2016-05-17 11:58:52 +0200

    Update 100doors.py

--- 100doors.py
+++ 100doors.py
@@ -1,8 +1,6 @@
 print("The # of the doors open after the 100th cycle: ", end="")
 
-doors = []
-for i in range(1, 101):
-    doors.append(0)
+doors = [0]*100
 
 for y in range(1, 101):
     for x in range(0,100):
